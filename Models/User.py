df0899b38cd6742372331cd6c012fe3fb7fdacaa
Darlan Ferreira <darlan.oliveiraferreira1@gmail.com>
2025-01-31 15:36:54 -0300

    update

--- Models/User.py
+++ Models/User.py
@@ -1,1 +1,10 @@
-# User.py
+from pydantic import BaseModel, Field
+from typing import Optional, List
+
+class User(BaseModel):
+	id: Optional[int] = Field(None, alias='_id')
+	name: str
+	email: str
+	password: str
+	address: str
+	phoneNumber: str
